b333cbea5a51fd012691a3c9499e537c48c5f706
Aevar <skrekkur@gmail.com>
2019-05-06 23:28:55 +0000

    docker test stuff

--- section5/nodestuff/frontend/src/App.js
+++ section5/nodestuff/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to EXOT.
+          Edit <code>src/App.js</code> and sav e to EXOT.
           And made some more changes
           I have started hoho
         </p>
